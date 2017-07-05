937452b61bc2b829ee005aed35a7715221c23975
Justin Holmes <justinmichaelholmes@gmail.com>
2017-07-05 16:58:19 -0600

    make sure html reports can use CSS

--- configuration/init.groovy.d/configure-jenkins.groovy
+++ configuration/init.groovy.d/configure-jenkins.groovy
@@ -15,5 +15,9 @@ if ( Jenkins.instance.views.findAll{ view -> view instanceof com.smartcodeltd.je
   Jenkins.instance.addView(view)
 }
 
+// support custom CSS for htmlreports
+// https://stackoverflow.com/questions/35783964/jenkins-html-publisher-plugin-no-css-is-displayed-when-report-is-viewed-in-j
+System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "")
+
 // This is a helper to delete views in the Jenkins script console if needed
 // Jenkins.instance.views.findAll{ view -> view instanceof com.smartcodeltd.jenkinsci.plugins.buildmonitor.BuildMonitorView }.each{ view -> Jenkins.instance.deleteView( view ) }
